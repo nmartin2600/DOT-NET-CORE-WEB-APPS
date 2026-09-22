3e30cad33a8065f93dbe2cd356450221be6835e0
Nicholas Martin <57820367+nmartin2600@users.noreply.github.com>
2026-09-22 16:55:36 -0400

    Complete HTML Submission Assignment

---
+++ HTMLAssignment/HTMLAssignment/wwwroot/js/site.js
@@ -0,0 +1,22 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var id = null;
+function myMove() {
+    var elem = document.getElementById("myAnimation");
+    var pos = 0;
+    clearInterval(id);
+    id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+myMove();
